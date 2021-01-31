a4f123712d31a40481edcab21b7e1671ddc0e740
Matteo Poggi <matteo.poggi.fi@gmail.com>
2021-01-31 01:17:37 +0100

    Add unit test for force.c. Add test for pbc.

--- src/common/test_common.cpp
+++ src/common/test_common.cpp
@@ -3,6 +3,30 @@
 
 #include <time.h>
 
+
+constexpr int SIZE = 20;
+
+class azzero_test: public ::testing::Test {
+
+protected:
+
+	const int size = SIZE;
+	double* buf;
+
+	void SetUp()
+		{
+			buf = new double[size];
+			for (int i = 0; i < size; ++i)
+				buf[i] = static_cast<double>(i + 1);
+		}
+
+	void TearDown()
+		{
+			delete[] buf;
+		}
+};
+
+
 TEST(test_wallclock, one_second)
 {
 	struct timespec t;
@@ -14,4 +38,15 @@ TEST(test_wallclock, one_second)
 	t_test += wallclock();
 	ASSERT_TRUE((t_test > 0.09));
 	ASSERT_TRUE((t_test < 0.11));
+}
+
+
+TEST_F(azzero_test, doubles)
+{
+	ASSERT_EQ(size, SIZE);
+        for (int i = 0; i < size; ++i)
+		ASSERT_DOUBLE_EQ(buf[i], static_cast<double>(i + 1));
+	azzero(buf, size);
+	for (int i = 0; i < size; ++i)
+		ASSERT_DOUBLE_EQ(buf[i], 0.0);
 }
---
+++ src/physics/test_force.cpp
@@ -0,0 +1,18 @@
+#include "gtest/gtest.h"
+
+extern "C" {
+	//#include "force.c"
+}
+/*
+TEST(pbc_test, inside)
+{
+	ASSERT_DOUBLE_EQ(pbc(3.0, 10.0 * 0.5), 3.0);
+	ASSERT_DOUBLE_EQ(pbc(-2.5, 10.0 * 0.5), -2.5);
+}
+
+TEST(pbc_test, outside)
+{
+	ASSERT_DOUBLE_EQ(pbc(7.0, 10.0 * 0.5), -3.0);
+	ASSERT_DOUBLE_EQ(pbc(-8.0, 10.0 * 0.5), 2.0);
+}
+*/
