966d9affa15b42dd2e88ffcfeb2fd14fdd70c2e3
Matteo Poggi <matteo.poggi.fi@gmail.com>
2021-02-10 10:09:29 +0100

    Add cell.c and test_cell.cpp for cell-list support.

--- src/physics/cell.c
+++ src/physics/cell.c
@@ -3,36 +3,63 @@
 #include "physics.h"
 
 /* (3^3 - 1) / 2 */
-#define HALFNEIGH 13
+#define HALFNEIGH 14
 
+static int pair_number(int ncs) {
+	switch (ncs) {
+	case 1:
+		return 1;
+	case 2:
+		return 36;
+	default:
+		return HALFNEIGH * ncs * ncs * ncs;
+	}
+}
 
 static void build_pairs(mdsys_t *sys) {
 	/* Toric fan generating the positive cone */
-	int fan[HALFNEIGH][3] = {{ 1,  0,  0},
-				{ 0,  1,  0},
-				{ 0,  0,  1},
-				{ 1,  1,  0},
-				{ 1,  0,  1},
-				{ 0,  1,  1},
-				{ 1,  1,  1},
-				{ 1, -1,  0},
-				{ 1,  0, -1},
-				{ 0,  1, -1},
-				{-1,  1,  1},
-				{ 1, -1,  1},
-				{ 1,  1,  1}};
+	int fan[HALFNEIGH][3] = {{ 0,  0,  0},
+				 { 1,  0,  0},
+				 { 0,  1,  0},
+				 { 0,  0,  1},
+				 { 1,  1,  0},
+				 { 1,  0,  1},
+				 { 0,  1,  1},
+				 { 1,  1,  1},
+				 { 1, -1,  0},
+				 { 1,  0, -1},
+				 { 0,  1, -1},
+				 {-1,  1,  1},
+				 { 1, -1,  1},
+				 { 1,  1, -1}};
 
-	int i, j, k, ii, jj, kk, ll;
+	
+
 	int ncellside = sys->ncellside;
+	if (ncellside == 1) {
+		sys->cellpairs[0] = 0;
+		sys->cellpairs[1] = 0;
+		return;
+	} 
+	if (ncellside == 2) {
+		for (int l = 0, p = 0; l < 8; ++l)
+			for (int ll = l; ll < 8; ++ll) {
+				sys->cellpairs[p++] = l;
+				sys->cellpairs[p++] = ll;
+			}
+		return;
+	}
+	
+	int i, j, k, ii, jj, kk, ll;	
 	int *pairptr = sys->cellpairs;
 	for (int l = 0; l < ncellside * ncellside * ncellside; ++l) {
 		k = l / (ncellside * ncellside);
 		j = (l % (ncellside * ncellside)) / ncellside;
 		i = (l % (ncellside * ncellside)) % ncellside;
 		for (int neig = 0; neig < HALFNEIGH; ++neig) {
-			ii = (i + fan[neig][0]) % ncellside;
-			jj = (j + fan[neig][1]) % ncellside;
-			kk = (k + fan[neig][2]) % ncellside;
+			ii = (i + fan[neig][0] + ncellside) % ncellside;
+			jj = (j + fan[neig][1] + ncellside) % ncellside;
+			kk = (k + fan[neig][2] + ncellside) % ncellside;
 			ll = ii + jj * ncellside + kk * ncellside * ncellside; 
 			*(pairptr++) = l;
 			*(pairptr++) = ll;
@@ -47,8 +74,9 @@ void build_cells(mdsys_t *sys) {
 	
 	sys->ncellside = (int)(sys->box / (sys->rcut * tolerance));
 	sys->cellsize = sys->box / sys->ncellside;
-	int ncell = sys->ncellside * sys->ncellside * sys->ncellside;
-	sys->cells = (cell_t*)malloc(ncell * sizeof(cell_t));
-	sys->cellpairs = (int*)malloc(2 * HALFNEIGH * ncell * sizeof(int));
+	int npairs = pair_number(sys->ncellside);
+	int ncells = sys->ncellside * sys->ncellside * sys->ncellside;
+	sys->cells = (cell_t*)malloc(ncells * sizeof(cell_t));
+	sys->cellpairs = (int*)malloc(2 * npairs * sizeof(int));
 	build_pairs(sys);
 }
--- src/physics/test_cell.cpp
+++ src/physics/test_cell.cpp
@@ -1,7 +1,23 @@
+#include <iostream>
+#include <vector>
+#include <utility>
+#include <algorithm>
+
 #include "gtest/gtest.h"
 
 extern "C" {
 	#include "cell.c"
+}
+
+inline int min(int x, int y) {return x < y ? x : y;}
+inline int max(int x, int y) {return x < y ? y : x;}
+
+static int pbc_int(int x, int size) {
+	while (x > size / 2)
+		x -= size;
+	while (x < - size / 2)
+		x += size;
+	return x;
 }
 
 /* TODO: test build_paires for the following situation
@@ -18,3 +34,67 @@ extern "C" {
  * 21 22 23
  * 24 25 26
  */
+
+class build_pairs_test: public ::testing::TestWithParam<int> {
+
+protected:
+	
+	mdsys_t sys;
+	int size = GetParam();
+	int size3 = size * size * size;
+	
+	virtual void SetUp() override
+		{
+			sys.ncellside = size;
+			sys.cellpairs = new int[2 * HALFNEIGH * size3];
+		}
+
+	virtual void TearDown() override
+		{
+			delete[] sys.cellpairs;
+		}
+};
+
+
+TEST_P(build_pairs_test, fill)
+{
+	using vec_t = std::vector<std::pair<int, int>>;
+	vec_t there, here;
+	build_pairs(&sys);
+
+	int x, y, M, m, npairs;
+
+	npairs = pair_number(size);
+	
+	for (int l = 0; l < 2 * npairs; l += 2) {
+		x = sys.cellpairs[l];
+		y = sys.cellpairs[l + 1];
+		M = max(x, y);
+		m = min(x, y);
+		there.emplace_back(m, M);
+	}
+	
+	std::sort(there.begin(), there.end());
+
+	int i, j, k, ii, jj, kk, di, dj, dk;
+	for (int l = 0; l < size3; ++l) {
+		k = l / (size * size);
+		j = (l % (size * size)) / size;
+		i = (l % (size * size)) % size;
+		for (int ll = l; ll < size3; ++ll) {
+			kk = ll / (size * size);
+			jj = (ll % (size * size)) / size;
+			ii = (ll % (size * size)) % size;
+			di = pbc_int(i - ii, size);
+			dj = pbc_int(j - jj, size);
+			dk = pbc_int(k - kk, size);
+			if (di * di + dj * dj + dk * dk < 4) {
+				here.emplace_back(l, ll);
+			}
+		}
+	}
+}
+
+INSTANTIATE_TEST_SUITE_P(build_pairs_test_parametric,
+			 build_pairs_test,
+			 ::testing::Values(1, 2, 3, 4, 5));
